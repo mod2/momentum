6f9a26fc940456998c6886b90824c3ffa48c98a8
bencrowder <ben.crowder@gmail.com>
2016-06-13 12:31:38 -0600

    Updates admin

--- momentum/admin.py
+++ momentum/admin.py
@@ -4,7 +4,7 @@ from .models import Goal, Entry, Folder, Context
 
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ('name', 'target_amount', 'type', 'period', 'priority', 'status',)
+    list_display = ('name', 'context', 'target_amount', 'type', 'period', 'priority', 'status',)
 
 
 @admin.register(Entry)
